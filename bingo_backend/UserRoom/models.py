763bd44eb7b4babe97094f27d28ddb3a54670656
Yulun Du <yulundu2@illinois.edu>
2016-04-14 04:03:49 -0500

    user and room ready v1.0

---
+++ bingo_backend/UserRoom/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+#from rest_framework import serializers
+
+# Create your models here.
+
+class User(models.Model):
+    #user_id = models.BigIntegerField()
+    user_id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 100)
+    score = models.IntegerField(default = 0)
+
+class Room(models.Model):
+    room_id = models.AutoField(primary_key = True)
+    owner_id = models.IntegerField()
+    users = models.TextField()
+    board_id = models.IntegerField(default = 0)
+    name = models.CharField(max_length = 100)
+    game_status = models.BooleanField(default = False)
+    drawer = models.IntegerField(default = 0)
