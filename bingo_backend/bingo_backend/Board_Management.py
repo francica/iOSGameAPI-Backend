7ac1bb1b9face47a5b9c0e64ecd73b8ed8de6e9b
unknown <robertfinedore@gmail.com>
2016-04-21 11:49:18 -0500

    v2 of board management

--- bingo_backend/bingo_backend/Board_Management.py
+++ bingo_backend/bingo_backend/Board_Management.py
@@ -1,80 +1,120 @@
 __author__ = 'Robert'
 
 
-# need to understand what sort of access management is going to have
-# timestamp
-
-from Board import Board
+from Board_Segment import Board
 import json
-from datetime import datetime
-from more_itertools import unique_everseen
+from Pen import Pen
+
 
 
 class Board_Management:
-    def __init__(self, room_id):
+    def __init__(self):
 
         self.game_board={}
+        self.board_counter=0
+
+
+
+    def add_new_game_board(self, pen_color,pen_width,room_id):
+        """function is to be used to create a new game board for a new room"""
         self.game_board[room_id]=[]
 
+        new_pen=Pen(pen_color,pen_width)
 
-    def add_new_game_board(self, pen_color,pen_width,res_width,res_height, time_stamp,room_id):
-        """function is to be used to create a new game board for a new room"""
+        new_board=Board(new_pen,self.board_counter,room_id)
+        new_board.initialize_new_board()
 
-        new_board=Board(pen_color,pen_width,time_stamp,room_id)
-        new_board.initialize_new_board(res_width,res_height)
         self.game_board[room_id].append(new_board)
+        self.board_counter+=1
 
 
 
     def update_pen(self,json_obj):
-        """{'pen_color':[r,g,b],'room_id': x,'pen_width':y, 'time_stamp':'%Y-%m-%d %H:%M:%S.%f'}"""
+        """ {'pen_color':[r,g,b],'room_id': x,'pen_width': y} """
 
         json_str=json.loads(json_obj)
-        new_board=Board(json_str['pen_color'],json_str['pen_width'],json_str['time_stamp'],json_str['room_id'])
+        new_pen=Pen(json_str['pen_color'],json_str['pen_width'])
+        new_board=Board(new_pen,self.board_counter,json_str['room_id'])
+
         self.game_board[json_str['room_id']].append(new_board)
+        self.board_counter+=1
 
 
     def update_board(self,json_obj):
+        """{'room_id': x, 'start_point': (x,y), 'end_point': (x,y) """
 
         json_str=json.loads(json_obj)
         self.game_board[json_str['room_id']][len(self.game_board[json_str['room_id']])-1].update_board_pixel(json_str['start_point'],json_str['end_point'])
 
     def get_board(self,json_obj):
 
+        """{'room_id': x, 'board_id': y}"""
+
+        json_data={}
         json_str=json.loads(json_obj)
-        pixel_changed=self.find_boards(json_str['room_id'],json_str['time_stamp'])
-        return pixel_changed
+
+        start_point_list,end_point_list, pen_list,board_id_list=self.find_boards(json_str['room_id'],json_str['board_id'])
+
+        if start_point_list==-1:
+            return -1
+
+        else:
+
+            for index in range(0,len(board_id_list)):
+                json_data[board_id_list[index]]=(start_point_list[index],end_point_list[index]),pen_list[index].pen_color,pen_list[index].pen_width
+
+            json_str=json.dumps(json_data)
+
+            # returns a string in the format {"board_id":[[[start_point],[end_point]],[pen_color],"pen_width"], ...}
+            return json_str
 
 
-    def find_boards(self,room_id,time_stamp):
 
-        dt_timestamp=datetime.strptime(time_stamp,'%Y-%m-%d %H:%M:%S.%f')
+    def find_boards(self,room_id,board_id):
+        """helper function that returns all the board information from current board_id to latest board_id"""
 
-        pixels_changed=[]
-        filter_pixel_changed=[]
-        flag=False
+        # check to make sure that the board is not already the latest board
+        if board_id==self.game_board[room_id][len(self.game_board[room_id])-1].board_id:
 
-        for board in self.game_board[room_id]:
-            dt_board_id=datetime.strptime(board.board_id,'%Y-%m-%d %H:%M:%S.%f')
+            return -1
 
-            if dt_board_id>dt_timestamp:
-                for pixel in board.pixel_changed:
-                    pixels_changed.append(pixel)
+        board_diff=[]
+        start_point_list=[]
+        end_point_list=[]
+        pen_list=[]
+        board_id_list=[]
 
-        pixels_changed.reverse()
+        for board_index in range(0, len(self.game_board[room_id])):
 
-        for pixel in pixels_changed:
-            for filter_pixel in filter_pixel_changed:
-                if pixel.point == filter_pixel.point:
-                    flag=True
-                    continue
+            # get all the boards from the board_id to the latest board
+            if self.game_board[room_id][board_index].board_id==board_id:
+                board_diff=self.game_board[room_id][board_index+1:]
 
-            if flag!=True:
-                filter_pixel_changed.append(pixel)
-            else:
-                flag=False
 
-        return filter_pixel_changed
+        if len(board_diff)!=0:
+
+            for board in board_diff:
+
+                # the first index will be the start point and the second index will be the end point
+                start_point_list.append(board.pixel_changed[0])
+                end_point_list.append(board.pixel_changed[1])
+
+                pen_list.append(board.pen)
+                board_id_list.append(board.board_id)
+
+
+            return start_point_list,end_point_list,pen_list,board_id_list
+
+        # no boards to return error check
+        else:
+            return -1
+
+
+
+
+
+
+
 
 
 
---
+++ bingo_backend/bingo_backend/Pen.py
@@ -0,0 +1,11 @@
+__author__ = 'Robert'
+
+
+class Pen:
+
+    def __init__(self,pen_color,pen_width):
+
+        self.pen_color=pen_color
+        self.pen_width=pen_width
+
+
